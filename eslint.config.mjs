6e1f2285fcaee385a6a67719061f4b2afc95bcfb
tomycesped <elefantinto@gmail.com>
2025-02-28 15:17:46 -0300

    Initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@next/next/no-img-element": "off",
+      "@jsx-ally/alt-text": "off"
+    },
+  
+  }
 ];
 
 export default eslintConfig;
---
+++ src/app/components/LazyImage.tsx
@@ -0,0 +1,53 @@
+import { useRef, useEffect, useState } from "react";
+import type { ImgHTMLAttributes } from "react";
+
+type LazyImageProps = {
+  src: string;
+  onLazyLoad?: (img: HTMLImageElement) => void;
+};
+
+type Props = ImgHTMLAttributes<HTMLImageElement> & LazyImageProps;
+
+export function LazyImage({
+  src,
+  onLazyLoad,
+  ...imgProps
+}: Props): React.JSX.Element {
+  const node = useRef<HTMLImageElement>(null);
+  const [isLazyLoaded, setIsLazyLoaded] = useState(false);
+  const [currentSrc, setCurrentSrc] = useState(
+    "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4="
+  );
+
+  useEffect(() => {
+    if (isLazyLoaded) {
+      return;
+    }
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (!entry.isIntersecting || !node.current) {
+          return;
+        }
+
+        setCurrentSrc(src);
+        observer.disconnect();
+        setIsLazyLoaded(true);
+
+        if (typeof onLazyLoad === "function") {
+          onLazyLoad(node.current);
+        }
+      });
+    });
+
+    if (node.current) {
+      observer.observe(node.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [src, onLazyLoad, isLazyLoaded]);
+
+  return <img ref={node} src={currentSrc} {...imgProps} />;
+}
